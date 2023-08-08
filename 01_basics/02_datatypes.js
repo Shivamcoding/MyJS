9b33d1b70c6e81726591080aa028ac8095bb95e6
Shivam <90306155+Shivamcoding@users.noreply.github.com>
2023-08-08 06:05:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+
+let name = "shivam" //string datatype
+let age = 21 // number
+let isLoggedIn = false // boolean
+
+//number => 2 to power 52
+//bigInt for very large number
+// String => " "
+// boolean => True/ False
+// null (stand alone value) typeof is object
+let state = null // is also a value
+
+// undefined means not defined
+// symbol => unique
+
+
+//object
+
+console.log(typeof "shivam");
