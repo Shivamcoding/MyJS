5bd15878f9a2aa69eec61779e9ac5fea40ebaed2
Shivam <90306155+Shivamcoding@users.noreply.github.com>
2023-08-08 07:45:41 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,5 +19,34 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//************************** Operations ***********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " shivam"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("2" + 1);
+// console.log(2 + "1");
+// console.log(2 + 2 + "1");
+// console.log("2" + 1 + 1);
+
+let gameCounter = 100
+gameCounter++; //PostFix increment
+++gameCounter; //PreFix increment
+console.log(gameCounter);
