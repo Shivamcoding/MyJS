02f5623eebc40c62131c43c5aea7349c61ad5c3b
Shivam <90306155+Shivamcoding@users.noreply.github.com>
2023-09-04 09:43:29 +0000

    Math and Nums

---
+++ 01_basics/05_nums_nd_math.js
@@ -0,0 +1,8 @@
+console.log(Math.random()); // 0-1 values
+
+console.log(Math.floor(3.9)); // 3.9 = 3, 4 = 4
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
