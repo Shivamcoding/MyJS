eb695065ed1116f7dda2a07a5eeb5123eb8f5305
Shivam <90306155+Shivamcoding@users.noreply.github.com>
2023-09-10 12:22:49 +0000

    iterations

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -9,3 +9,21 @@ if (username) {
 // falsy values : false, 0, -0, bigInt 0n, "", null, undefined, NaN
 
 // truthy values : "0", "false", " ", [], {}, function(){}
+
+// Nullish Coalesing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+val1 = null ?? 10
+// val1 = undefined ?? 10
+
+console.log(val1);
+
+
+// Terniary Operator
+
+//condition ? true : false
+
+const num = 100;
+
+num <= 80 ? console.log("less than 80") : console.log("More than 80")
---
+++ 05_iterations.js/one.js
@@ -0,0 +1,41 @@
+// for loop
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    // if(i===5) {
+    //     console.log("5 is best number")
+    // }
+    // console.log(element);
+}
+
+for(let i = 1; i <= 10; i++) {
+    //console.log(`Outer loop value: ${i}`)
+    for(let j = 0; j <= 10; j++) {
+        //console.log(`Inner loop value ${j} and Inner value ${i}`)
+       // console.log(i + '*' + j + ' = ' + i*j);
+    }
+}
+
+let myArray = ["flash", "batman", "superman"]
+for(let i = 0; i < myArray.length; i++) {
+    //console.log(myArray)
+    const element = myArray[i]
+    //console.log(element)
+}
+
+// break and continue
+
+for(let i = 1; i <= 20; i++) { 
+    if(i == 5) {
+        console.log(`Detected ${i}`)
+        break;
+    }
+   console.log (i)
+}
+
+for(let i = 1; i <= 20; i++) { 
+    if(i == 5) {
+        console.log(`Detected ${i}`)
+        continue;
+    }
+   console.log (i)
+}
