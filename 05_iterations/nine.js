c8df70741e976ded7c8e7cd89008cffb5f9788e9
Shivam <90306155+Shivamcoding@users.noreply.github.com>
2023-09-12 06:01:51 +0000

    Map,Reduce and Filter

---
+++ 05_iterations/nine.js
@@ -0,0 +1,12 @@
+// reduce
+
+const nums = [1,2,3,4]
+
+// const sum = nums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+// const sum = nums.reduce((acc, currval) => acc+currval, 0)
+//console.log(sum);
+
--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -23,4 +23,22 @@ myNum.forEach((num) => {
         newNums.push(num) // [ 5, 6, 7, 8, 9, 10 ]
     }
 })
-console.log(newNums);
+//console.log(newNums);
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1999, edition: 2001 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1980, edition: 1999 },
+    { title: 'Book Three', genre: 'Science', publish: 1997, edition: 2002 },
+    { title: 'Book Four', genre: 'History', publish: 2001, edition: 2007 },
+    { title: 'Book Five', genre: 'Science', publish: 2007, edition: 2010 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1990, edition: 2000 },
+    { title: 'Book Seven', genre: 'History', publish: 1989, edition: 1990 },
+    { title: 'Book Eight', genre: 'Non-Fiction', publish: 2009, edition: 2011 }
+]
+
+//const userBooks = books.filter( (bk) => bk.publish >= 1999 )
+
+const userBooks = books.filter( (bk) => {
+    return bk.publish >= 2000 && bk.genre === 'History'
+} )
+console.log(userBooks);
